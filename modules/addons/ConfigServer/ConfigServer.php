c4164e8eaf0a6a55304c5484a0765529e2717296
configserverpro <45038149+configserverpro@users.noreply.github.com>
2018-12-04 22:25:41 +0330

    Version update

--- modules/addons/ConfigServer/ConfigServer.php
+++ modules/addons/ConfigServer/ConfigServer.php
@@ -12,7 +12,7 @@ function ConfigServer_config()
     $configArray = array(
         "name" => "ConfigServer",
         "description" => "ConfigServer License Management module",
-        "version" => '1.0.3',
+        "version" => '1.0.5',
         "author" => "Amirhossein Matini",
         "language" => "english",
         "fields" => []
